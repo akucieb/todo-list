13cd8869b46bbf1fdac1f2a3a3bfd7a5631f2879
Anita Kucieba <akucieba@gmail.com>
2018-06-02 09:51:37 +0200

    initial commit

---
+++ src/main/java/pl/sda/poznan/service/TodoService.java
@@ -0,0 +1,23 @@
+package pl.sda.poznan.service;
+
+
+import java.util.List;
+import pl.sda.poznan.model.TodoItem;
+import pl.sda.poznan.repository.TodoRepository;
+
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<TodoItem> getAllTodos() {
+        return this.todoRepository.getAll();
+    }
+
+    public void save(TodoItem todoItem) {
+        this.todoRepository.save(todoItem);
+    }
+}
---
+++ src/main/java/pl/sda/poznan/servlets/TodoServlet.java
@@ -0,0 +1,40 @@
+package pl.sda.poznan.servlets;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import pl.sda.poznan.model.TodoItem;
+import pl.sda.poznan.repository.TodoRepository;
+import pl.sda.poznan.service.TodoService;
+import util.PersistenceUtil;
+
+@WebServlet(name = "TodoServlet", urlPatterns = {"/todo"})
+public class TodoServlet extends HttpServlet {
+
+    private TodoService todoService;
+
+    public TodoServlet() {
+        this.todoService = new TodoService(new TodoRepository(PersistenceUtil.getEntityManager()));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+//    todo: delete this
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTitle("Kupic kwiaty");
+        todoItem.setDescription("Fajny opis");
+        todoService.save(todoItem);
+
+        List<TodoItem> allTodos = todoService.getAllTodos();
+        req.setAttribute("todos", allTodos);
+        req
+                .getRequestDispatcher("/todo/index.jsp")
+                .forward(req, resp);
+    }
+}
